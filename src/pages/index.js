0ccb0ec58c0de7d3edc9f24103679c5f58342352
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-07-29 12:52:33 +0530

    added favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react"
 import "./index.scss"
+import SEO from "../components/seo"
 import MainPage from "../components/mainpage"
 
 const IndexPage = () => {
-  return <MainPage />
+  return (
+    <div>
+      <SEO title="Devansh Agarwal" />
+      <MainPage />
+    </div>
+  )
 }
 
 export default IndexPage
