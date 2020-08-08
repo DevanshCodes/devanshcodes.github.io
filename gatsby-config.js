ccaa6c0fba2e61ce99c35e4e35b542ea5112090d
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-08-08 20:23:12 +0530

    fixed the config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,11 +36,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Devansh Agarwal`,
+        short_name: `DevanshAg`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#171c28`,
+        theme_color: `#171c28`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
